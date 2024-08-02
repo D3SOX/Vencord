4e737c38ad29b9a48e8071cdf42c66bf83882555
D3SOX <nico@d3sox.me>
2024-08-02 10:07:16 +0200

    fix(betterActivities): avoid conditional hook calls
    Prevents crashing when the activities length changes

--- src/plugins-extra/betterActivities/index.tsx
+++ src/plugins-extra/betterActivities/index.tsx
@@ -332,14 +332,14 @@ export default definePlugin({
                 setCurrentActivity(activities[0]);
         }, [activities]);
 
-        if (!activities.length) return null;
-
         // we use these for other activities, it would be better to somehow get the corresponding activity props
         const generalProps = useMemo(() => Object.keys(props).reduce((acc, key) => {
             // exclude activity specific props to prevent copying them to all activities (e.g. buttons)
             if (key !== "renderActions" && key !== "application") acc[key] = props[key];
             return acc;
         }, {}), [props]);
+
+        if (!activities.length) return null;
 
         if (settings.store.allActivitiesStyle === "carousel") {
             return (
